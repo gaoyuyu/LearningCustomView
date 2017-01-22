e75b7d4d11de470184193ec640897e7d89b597e9
gaoyuyu <740514999@qq.com>
2017-01-22 16:42:37 +0800

    Add Animation

--- app/src/main/java/com/gaoyy/learningcustomview/view/CircleStatisticalView.java
+++ app/src/main/java/com/gaoyy/learningcustomview/view/CircleStatisticalView.java
@@ -25,6 +25,7 @@ public class CircleStatisticalView extends View
     private int currentAnimValue;
     private double[] datas = {0.3, 0.4, 0.15, 0.1, 0.05};
     private String[] texts = {"火影", "Ex-Aid", "扎克", "RxJava", "ReactNative"};
+    private float textSize = 0f;
 
     private int outRectWidth;
     private int outRectHeight;
@@ -248,48 +249,81 @@ public class CircleStatisticalView extends View
         valueAnimator.setDuration(5000);
         valueAnimator.start();
 
+        ValueAnimator valueAnimator1 = ValueAnimator.ofFloat(0f, 35f);
+        valueAnimator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+        {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator)
+            {
+                textSize = (float) valueAnimator.getAnimatedValue();
+                invalidate();
+            }
+        });
+        valueAnimator1.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator1.setDuration(2000);
+        valueAnimator1.start();
+
+
+
     }
 
 
     //画文字
-    private void drawText(Canvas mCanvas, float textAngle, String kinds, float needDrawAngle, int mRadius)
+    private void drawText(Canvas mCanvas, float end, String kinds, float needDrawAngle, int mRadius)
     {
         Rect rect = new Rect();
         mPaint.setColor(Color.BLACK);
-        mPaint.setTextSize(35f);
+        mPaint.setTextSize(textSize);
         mPaint.getTextBounds(kinds, 0, kinds.length(), rect);
-        if (textAngle >= 0 && textAngle <= 90)
+        if (end >= 0 && end <= 90)
         { //画布坐标系第一象限(数学坐标系第四象限)
 //            if (needDrawAngle < 30) { //如果小于某个度数,就把文字画在饼状图外面
-            mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(textAngle))),
-                    (float) (mRadius * 1.2 * Math.sin(Math.toRadians(textAngle))) + rect.height() / 2, mPaint);
+            mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(end))),
+                    (float) (mRadius * 1.2 * Math.sin(Math.toRadians(end))) + rect.height() / 2, mPaint);
+
+            Log.i(TAG,"画布坐标系第一象限(数学坐标系第四象限)");
+            Log.i(TAG,"end-->"+end);
+            Log.i(TAG,"x--->"+(float) (mRadius * 1.2 * Math.cos(Math.toRadians(end))));
+            Log.i(TAG,"y--->"+(float) (mRadius * 1.2 * Math.sin(Math.toRadians(end))) + rect.height() / 2);
 //            } else {
 //                mCanvas.drawText(kinds, (float) (mRadius * 0.75 * Math.cos(Math.toRadians(textAngle))), (float) (mRadius * 0.75 * Math.sin(Math.toRadians(textAngle)))+rect.height()/2, mPaint);
 //            }
         }
-        else if (textAngle > 90 && textAngle <= 180)
+        else if (end > 90 && end <= 180)
         { //画布坐标系第二象限(数学坐标系第三象限)
 //            if (needDrawAngle < 30) {
-            mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(180 - textAngle))),
-                    (float) (mRadius * 1.2 * Math.sin(Math.toRadians(180 - textAngle))) + rect.height() / 2, mPaint);
+            mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(180 - end))),
+                    (float) (mRadius * 1.2 * Math.sin(Math.toRadians(180 - end))) + rect.height() / 2, mPaint);
+            Log.e(TAG,"画布坐标系第二象限(数学坐标系第三象限)");
+            Log.e(TAG,"end-->"+end);
+            Log.e(TAG,"x--->"+(float) (-mRadius * 1.2 * Math.cos(Math.toRadians(180 - end))));
+            Log.e(TAG,"y--->"+(float) (mRadius * 1.2 * Math.sin(Math.toRadians(180 - end))) + rect.height() / 2);
 //            } else {
 //                mCanvas.drawText(kinds, (float) (-mRadius * 0.75 * Math.cos(Math.toRadians(180 - textAngle))), (float) (mRadius * 0.75 * Math.sin(Math.toRadians(180 - textAngle)))+rect.height()/2, mPaint);
 //            }
         }
-        else if (textAngle > 180 && textAngle <= 270)
+        else if (end > 180 && end <= 270)
         { //画布坐标系第三象限(数学坐标系第二象限)
 //            if (needDrawAngle < 30) {
-            mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(textAngle - 180))),
-                    (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(textAngle - 180))) + rect.height() / 2, mPaint);
+            mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(end - 180))),
+                    (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(end - 180))) + rect.height() / 2, mPaint);
 //            } else {
 //                mCanvas.drawText(kinds, (float) (-mRadius * 0.75 * Math.cos(Math.toRadians(textAngle - 180))), (float) (-mRadius * 0.75 * Math.sin(Math.toRadians(textAngle - 180)))+rect.height()/2, mPaint);
 //            }
+            Log.w(TAG,"画布坐标系第三象限(数学坐标系第二象限)");
+            Log.w(TAG,"end-->"+end);
+            Log.w(TAG,"x--->"+(float) (-mRadius * 1.2 * Math.cos(Math.toRadians(end - 180))));
+            Log.w(TAG,"y--->"+(float) (-mRadius * 1.2 * Math.sin(Math.toRadians(end - 180))) + rect.height() / 2);
         }
         else
         { //画布坐标系第四象限(数学坐标系第一象限)
 //            if (needDrawAngle < 30) {
-            mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(360 - textAngle))),
-                    (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(360 - textAngle))) + rect.height() / 2, mPaint);
+            mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(360 - end))),
+                    (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(360 - end))) + rect.height() / 2, mPaint);
+            Log.d(TAG,"画布坐标系第四象限(数学坐标系第一象限)");
+            Log.d(TAG,"end-->"+end);
+            Log.d(TAG,"x--->"+(float) (mRadius * 1.2 * Math.cos(Math.toRadians(360 - end))));
+            Log.d(TAG,"y--->"+(float) (-mRadius * 1.2 * Math.sin(Math.toRadians(360 - end))) + rect.height() / 2);
 //            } else {
 //                mCanvas.drawText(kinds, (float) (mRadius * 0.75 * Math.cos(Math.toRadians(360 - textAngle))), (float) (-mRadius * 0.75 * Math.sin(Math.toRadians(360 - textAngle)))+rect.height()/2, mPaint);
 //            }
