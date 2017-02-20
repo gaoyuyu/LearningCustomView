bb0a9f576f03123102199db948bdf8db0806b6ec
gaoyuyu <740514999@qq.com>
2017-02-21 00:46:01 +0800

    Add Animator

--- app/src/main/java/com/gaoyy/learningcustomview/ui/ColorfulBarActivity.java
+++ app/src/main/java/com/gaoyy/learningcustomview/ui/ColorfulBarActivity.java
@@ -2,16 +2,131 @@ package com.gaoyy.learningcustomview.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.gaoyy.learningcustomview.R;
+import com.gaoyy.learningcustomview.view.AnimItem;
 
 public class ColorfulBarActivity extends AppCompatActivity
 {
+//    private static final String TAG = ColorfulBarActivity.class.getSimpleName();
+//    private LinearLayout activityColorfulBar;
+//    private ImageView iconImg;
+//    private TextView iconText;
+//    private Button reset;
+//    AnimatorSet animatorSet = new AnimatorSet();
+//
+//    private void assignViews() {
+//        activityColorfulBar = (LinearLayout) findViewById(R.id.activity_colorful_bar);
+//        iconImg = (ImageView) findViewById(R.id.icon_img);
+//        iconText = (TextView) findViewById(R.id.icon_text);
+//        reset = (Button) findViewById(R.id.reset);
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colorful_bar);
+        final AnimItem animItem = (AnimItem) findViewById(R.id.item);
+        Button start = (Button)findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                animItem.startAnim();
+            }
+        });
+        Button reverse = (Button)findViewById(R.id.reverse);
+        reverse.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                animItem.reverseAnim();
+            }
+        });
+
+
+
+        /*
+        assignViews();
+
+
+        activityColorfulBar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                ObjectAnimator textScaleAnim1 = ObjectAnimator
+                        .ofFloat(iconText, "ScaleX", 0.3F, 1.0F)
+                        .setDuration(500);
+                ObjectAnimator textScaleAnim2 = ObjectAnimator
+                        .ofFloat(iconText, "ScaleY", 0.3F, 1.0F)
+                        .setDuration(500);
+                ObjectAnimator iconScaleAnim1 = ObjectAnimator
+                        .ofFloat(iconImg, "ScaleX", 1.0F, 0.8F)
+                        .setDuration(500);
+                ObjectAnimator iconScaleAnim2 = ObjectAnimator
+                        .ofFloat(iconImg, "ScaleY", 1.0F, 0.8F)
+                        .setDuration(500);
+
+                ObjectAnimator textAlphaAnim = ObjectAnimator.ofFloat(iconText,"Alpha",iconText.getAlpha(),1.0f).setDuration(500);
+
+                ObjectAnimator yTextAnim = ObjectAnimator.ofFloat(iconText,"TranslationY",-iconImg.getMeasuredHeight()*0.2f).setDuration(500);
+
+
+
+                animatorSet.playTogether(textScaleAnim1,textScaleAnim2,iconScaleAnim1,iconScaleAnim2,yTextAnim,textAlphaAnim);
+
+
+                animatorSet.addListener(new AnimatorListenerAdapter()
+                {
+                    @Override
+                    public void onAnimationStart(Animator animation)
+                    {
+                        super.onAnimationStart(animation);
+                        iconText.setVisibility(View.VISIBLE);
+                    }
+                });
+
+
+                animatorSet.start();
+            }
+        });
+
+
+
+        reset.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                ObjectAnimator textScaleAnim1 = ObjectAnimator
+                        .ofFloat(iconText, "ScaleX", iconText.getScaleX(), 0.0F)
+                        .setDuration(500);
+                ObjectAnimator textScaleAnim2 = ObjectAnimator
+                        .ofFloat(iconText, "ScaleY", iconText.getScaleY(), 0.0F)
+                        .setDuration(500);
+                ObjectAnimator iconScaleAnim1 = ObjectAnimator
+                        .ofFloat(iconImg, "ScaleX",  iconImg.getScaleX(),1.0F)
+                        .setDuration(500);
+                ObjectAnimator iconScaleAnim2 = ObjectAnimator
+                        .ofFloat(iconImg, "ScaleY", iconImg.getScaleY(),1.0F)
+                        .setDuration(500);
+                ObjectAnimator textAlphaAnim = ObjectAnimator.ofFloat(iconText,"Alpha",iconText.getAlpha(),0.0f).setDuration(500);
+                ObjectAnimator yTextAnim = ObjectAnimator.ofFloat(iconText,"TranslationY",iconImg.getMeasuredHeight()*0.2f).setDuration(500);
+
+                animatorSet.playTogether(textScaleAnim1,textScaleAnim2,iconScaleAnim1,iconScaleAnim2,textAlphaAnim,yTextAnim);
+                animatorSet.start();
+
+            }
+        });
+
+        */
+
     }
 }
--- app/src/main/java/com/gaoyy/learningcustomview/view/AnimItem.java
+++ app/src/main/java/com/gaoyy/learningcustomview/view/AnimItem.java
@@ -1,159 +1,146 @@
 package com.gaoyy.learningcustomview.view;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v7.widget.AppCompatDrawableManager;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.gaoyy.learningcustomview.R;
 
 /**
- * Created by gaoyy on 2017/2/19.
+ * Created by gaoyy on 2017/2/21.
  */
 
-public class AnimItem extends View implements View.OnClickListener
+public class AnimItem extends LinearLayout
 {
-    private static final String TAG = AnimItem.class.getSimpleName();
-    private Paint mPaint;
+    private ImageView icon;
+    private TextView text;
+    private AnimatorSet animatorSet = new AnimatorSet();
 
-    private float textSize = 0f;
-
-    public float getTextSize()
-    {
-        return textSize;
-    }
-
-    public void setTextSize(float textSize)
-    {
-        this.textSize = textSize;
-    }
 
     public AnimItem(Context context)
     {
-        this(context, null);
+        this(context,null);
     }
 
     public AnimItem(Context context, AttributeSet attrs)
     {
-        this(context, attrs, -1);
+        this(context, attrs,-1);
     }
 
     public AnimItem(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
-        initPaint();
-        setOnClickListener(this);
+        init();
+        initChildViews(context);
     }
+
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public AnimItem(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes)
     {
         super(context, attrs, defStyleAttr, defStyleRes);
-        initPaint();
-        setOnClickListener(this);
+        init();
+        initChildViews(context);
     }
 
-    private void initPaint()
+    private void initChildViews(Context context)
     {
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setStrokeWidth(10f);
-        mPaint.setStyle(Paint.Style.FILL);
+        setOrientation(VERTICAL);
+        setGravity(Gravity.CENTER);
+        icon  =  new ImageView(context);
+        icon.setImageDrawable(getResources().getDrawable(R.mipmap.ic_launcher));
+        icon.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+
+        text = new TextView(context);
+        text.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+        text.setTextSize(16);
+        text.setText("Darkness");
+        text.setTextColor(getResources().getColor(R.color.colorAccent));
+
+        addView(icon,0);
+        addView(text,1);
     }
 
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    public void startAnim()
     {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        Log.i(TAG, "padding bottom-->" + getPaddingBottom());
-    }
+        ObjectAnimator textScaleAnim1 = ObjectAnimator
+                .ofFloat(text, "ScaleX", 0.3F, 1.0F)
+                .setDuration(500);
+        ObjectAnimator textScaleAnim2 = ObjectAnimator
+                .ofFloat(text, "ScaleY", 0.3F, 1.0F)
+                .setDuration(500);
+        ObjectAnimator iconScaleAnim1 = ObjectAnimator
+                .ofFloat(icon, "ScaleX", 1.0F, 0.8F)
+                .setDuration(500);
+        ObjectAnimator iconScaleAnim2 = ObjectAnimator
+                .ofFloat(icon, "ScaleY", 1.0F, 0.8F)
+                .setDuration(500);
 
-    @Override
-    protected void onDraw(Canvas canvas)
-    {
-        super.onDraw(canvas);
+        ObjectAnimator textAlphaAnim = ObjectAnimator.ofFloat(text,"Alpha",text.getAlpha(),1.0f).setDuration(500);
 
-        mPaint.setColor(getResources().getColor(R.color.flower));
-
-
-
-        Bitmap icon = ((BitmapDrawable)getResources().getDrawable(R.mipmap.ic_flower)).getBitmap();
-        int iconWidth = icon.getWidth();
-        int iconHeight = icon.getHeight();
-        Log.i(TAG,"iconWidth-->"+iconWidth);
-        Log.i(TAG,"iconHeight-->"+iconHeight);
-
-
-        String text = "Friend";
-        Rect textRect = new Rect();
-        mPaint.getTextBounds(text,0,text.length(),textRect);
-
-        Log.i(TAG,"text width-->"+textRect.width());
-        Log.i(TAG,"text height-->"+textRect.height());
+        ObjectAnimator yTextAnim = ObjectAnimator.ofFloat(text,"TranslationY",-icon.getMeasuredHeight()*0.2f).setDuration(500);
 
 
 
-        mPaint.setTextSize(35f);
-        canvas.drawText("Friend",0,iconHeight+textRect.height(),mPaint);
+        animatorSet.playTogether(textScaleAnim1,textScaleAnim2,iconScaleAnim1,iconScaleAnim2,yTextAnim,textAlphaAnim);
 
 
-        canvas.drawBitmap(icon, 0, 0, mPaint);
-    }
-
-    @Override
-    public void onClick(View view)
-    {
-        Log.e(TAG,"===onClick===");
-//        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f,35f);
-//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
-//        {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator valueAnimator)
-//            {
-//                textSize = (float)valueAnimator.getAnimatedValue();
-//                setTextSize(textSize);
-//                invalidate();
-//            }
-//        });
-        ScaleAnimation animation =new ScaleAnimation(0.0f, 1.4f, 0.0f, 1.4f,
-                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        animation.setDuration(2000);
-        animation.start();
-    }
-
-    /**
-     * VectorDrawable转Bitmap
-     * @param context
-     * @param drawableId
-     * @return
-     */
-    public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId)
-    {
-        Drawable drawable = AppCompatDrawableManager.get().getDrawable(context, drawableId);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+        animatorSet.addListener(new AnimatorListenerAdapter()
         {
-            drawable = (DrawableCompat.wrap(drawable)).mutate();
+            @Override
+            public void onAnimationStart(Animator animation)
+            {
+                super.onAnimationStart(animation);
+                text.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        animatorSet.start();
+    }
+
+    public void reverseAnim()
+    {
+        ObjectAnimator textScaleAnim1 = ObjectAnimator
+                .ofFloat(text, "ScaleX", text.getScaleX(), 0.0F)
+                .setDuration(500);
+        ObjectAnimator textScaleAnim2 = ObjectAnimator
+                .ofFloat(text, "ScaleY", text.getScaleY(), 0.0F)
+                .setDuration(500);
+        ObjectAnimator iconScaleAnim1 = ObjectAnimator
+                .ofFloat(icon, "ScaleX",  icon.getScaleX(),1.0F)
+                .setDuration(500);
+        ObjectAnimator iconScaleAnim2 = ObjectAnimator
+                .ofFloat(icon, "ScaleY", icon.getScaleY(),1.0F)
+                .setDuration(500);
+        ObjectAnimator textAlphaAnim = ObjectAnimator.ofFloat(text,"Alpha",text.getAlpha(),0.0f).setDuration(500);
+        ObjectAnimator yTextAnim = ObjectAnimator.ofFloat(text,"TranslationY",icon.getMeasuredHeight()*0.2f).setDuration(500);
+
+        animatorSet.playTogether(textScaleAnim1,textScaleAnim2,iconScaleAnim1,iconScaleAnim2,textAlphaAnim,yTextAnim);
+        animatorSet.start();
+    }
+
+
+
+    private void init()
+    {
+        //使用isInEditMode解决可视化编辑器无法识别自定义控件的问题
+        if (isInEditMode())
+        {
+            return;
         }
-
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bitmap;
     }
 }
